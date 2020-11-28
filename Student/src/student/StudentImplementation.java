bc2b4dbb9460e473bffae06aae8ebf14b163cd47
josemibnf <josemi.bnf@outlook.es>
2020-11-28 15:14:43 +0100

    ,,

--- Student/src/student/StudentImplementation.java
+++ Student/src/student/StudentImplementation.java
@@ -3,6 +3,8 @@ package student;
 import java.rmi.server.UnicastRemoteObject;
 
 public class StudentImplementation extends UnicastRemoteObject implements StudentInterface{
+    List<String> questions = new ArrayList<String>();
+    String universityID;
 
     public StudentImplementation(){
         
