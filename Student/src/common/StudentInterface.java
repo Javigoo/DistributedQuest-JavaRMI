c429b7b851468fb11ac3bdce2e9dd4537b8ce0e9
josemibnf <josemi.bnf@outlook.es>
2020-11-28 14:38:24 +0100

    ...

--- Student/src/common/StudentInterface.java
+++ Student/src/common/StudentInterface.java
@@ -4,4 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface StudentInterface extends Remote {
+
+    public void setNextQuestion(List<String> question) throws RemoteException;
+    public void setGrade(Integer grade) throws RemoteException;
 }
--- Student/src/student/StudentImplementation.java
+++ Student/src/student/StudentImplementation.java
@@ -1,5 +1,19 @@
 package student;
 
-public class StudentImplementation {
+import java.rmi.server.UnicastRemoteObject;
+
+public class StudentImplementation extends UnicastRemoteObject implements StudentInterface{
+
+    public StudentImplementation(){
+        
+    }
+
+    public void setNextQuestion(List<String> question) throws RemoteException{
+
+    }
+
+    public void setGrade(Integer grade) throws RemoteException{
+
+    }
     
 }
