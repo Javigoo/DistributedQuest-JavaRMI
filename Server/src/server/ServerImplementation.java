c3f9610dfc6d2bbedd037a8958324e9cf70121fc
josemibnf <josemi.bnf@outlook.es>
2020-11-28 12:24:34 +0100

    created server dir

---
+++ Server/src/server/ServerImplementation.java
@@ -0,0 +1,9 @@
+package server;
+import java.rmi.server.UnicastRemoteObject;
+
+import common.ServerInterface;
+import common.ClientInterface;
+
+public class ServerImplementation extends UnicastRemoteObject implements ServerInterface {
+    
+}
