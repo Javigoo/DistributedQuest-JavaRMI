f84b02eea9cbaeeaed0a43b3e46f05c1b6976d3c
josemibnf <josemi.bnf@outlook.es>
2020-11-28 15:59:20 +0100

    logica del profe

--- Profesor/src/profesor/Profesor.java
+++ Profesor/src/profesor/Profesor.java
@@ -1,5 +1,40 @@
 public class Profesor {
+    private static Registry startRegistry(Integer port) throws RemoteException {
+        if (port == null) {
+            port = 1099;
+        }
+        try {
+            Registry registry = LocateRegistry.getRegistry(port);
+            registry.list();
+            // The above call will throw an exception
+            // if the registry does not already exist
+            return registry;
+        } catch (RemoteException ex) {
+            // No valid registry at that port.
+            System.out.println("RMI registry cannot be located ");
+            Registry registry = LocateRegistry.createRegistry(port);
+            System.out.println("RMI registry created at port ");
+            return registry;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        try {
+            Registry registry = startRegistry(null);
+            ProfesorImplementation obj = new ProfesorImplementation();
+            try {
+                registry.bind("Profesor", (Remote) obj);
+            } catch (Exception e){
+                System.err.println("Server exception: " + e.toString()); e.printStackTrace();
+            }
+
+            try {
+                // LOGICA DE LA APLICACION DEL PROFE.
+            } catch (InterruptedException e) {
+                System.err.println("Server exception: " + e.toString()); e.printStackTrace();
+            }
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString()); e.printStackTrace();
+        }
     }
 }
