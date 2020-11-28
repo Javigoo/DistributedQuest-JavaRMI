e3b74ae3e57686554a17e72e2dfde8daba670028
josemibnf <josemi.bnf@outlook.es>
2020-11-28 13:59:57 +0100

    --

--- Server/src/common/ProfesorInterface.java
+++ Server/src/common/ProfesorInterface.java
@@ -4,6 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ProfesorInterface extends Remote{
-    public void registerStudent(ClientInterface client) throws RemoteException;
-    public void sendNumber(ClientInterface client, int number) throws RemoteException;
+    public void registerStudent(StudentInterface student) throws RemoteException;
+    public void responseAnAnswer(StudentInterface student, Integer choice) throws RemoteException;
 }
--- Server/src/server/ProfesorImplementation.java
+++ Server/src/server/ProfesorImplementation.java
@@ -1,17 +1,108 @@
 package server;
+import java.io.File;
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 import common.ProfesorInterface;
 import common.StudentInterface;
 
-public class ProfesorImplementation extends UnicastRemoteObject implements ServerInterface {
+private class Exam {
+    private class Question {
+        List<String> choices = new ArrayList<String>();
+        Integer correctChoice = None;
 
-    public void uploadExam(){
+        public question(List<String> choices, Integer correctChoice){
+            if (choices.length()>=correctChoice){
+                throw Exception;
+            }
+            this.choices = choices;
+            this.correctChoice = correctChoice;
+        }
+
+        public Boolean isTheCorrectResponse(Integer response){
+            if (response==this.correctChoice){
+                return true;
+            }
+            return false;
+        }
 
     }
 
-    public void startExam(){}
+    List<Question> questions = new ArrayList<Question>();
 
+    public Exam(){}
+
+    public void uploadCSV(File csv){
+        while( True){
+            List<String> choices = new ArrayList<String>();
+            Integer correctChoice = new Interger();
+            while (True) {
+                // Va leyendo el csv ...
+            }
+            try{
+                Question question = new Question(choices, correctChoice);
+            } catch (Exception e){
+                System.err.println("remote exception: " + e.toString()); e.printStackTrace();
+            }
+            this.questions.add(question);
+        }
+    }
+
+    public Boolean isCorrectAnswer(Integer question, Integer response){
+        return this.questions[question].isTheCorrectResponse(response);
+    }
+
+    public List<String> getNextQuestion(){
+        return this.questions.next();
+    }
+}
+
+public class ProfesorImplementation extends UnicastRemoteObject implements ServerInterface {
+    private HashMap<StudentInterface, List<Integer>> studentResponses = new HashMap<>();
+    private Exam exam = new Exam();
+
+    public ProfesorImplementation() throws RemoteException {
+        super();
+    }
+
+    public void registerStudent(StudentInterface student) throws RemoteException {
+        this.studentResponses.add(student); // ¿Se guarda el universityID fuera del objeto?
+    }
+
+    public void responseAnAnswer(StudentInterface student, Integer choice) throws RemoteException {
+        this.responses[student].add(choice);
+    }
+
+    public void sendNextQuestion() {
+        List<String> question = Exam.getNextQuestion();
+        for (int i=0; i<this.students.length(); i++){
+            this.students[i].setNextQuestion(question);
+        }
+    }
+
+    public void sendGrade(StudentInterface student){
+        List<Integer> responses = this.studentResponses[student];
+        Integer grade = 0;
+        for (int i=0; i<responses.length(); i++){
+            if (this.exam.isCorrectAnswer(i, responses[i])==true){
+                grade++;
+            }
+        }
+        student.setGrade(grade);
+    }
+
+    public void uploadExam(File csv){
+        this.exam.uploadCSV(csv);
+    }
+
+    public void startExam(){} // Now, the students can join to the room.
+
+    public void beginExam(){
+    } // Now, the students can no longer join the room.
     
-    
+
+    public void finishExam(){
+    } // The profesor decides to finish the exam.
+
 }
