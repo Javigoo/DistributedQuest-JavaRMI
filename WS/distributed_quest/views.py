ccf1f974f84ef8495dfab60deb393b0bc116b855
javigoo <javierroiggregorio@gmail.com>
2021-01-08 16:24:13 +0100

    typo

--- WS/distributed_quest/views.py
+++ WS/distributed_quest/views.py
@@ -1,6 +1,15 @@
 from rest_framework import generics, filters
 from .models import *
 from .serializers import *
+from rest_framework import status
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from django.views.generic import View
+
 
 # Create your views here.
 class ExamList(generics.ListCreateAPIView):
@@ -8,17 +17,17 @@ class ExamList(generics.ListCreateAPIView):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
 
-class ExamDetail(ApiView):
+class ExamDetail(APIView):
     def get(self, request, key, format=None):
-        return Response(Exams.objects.get(key=key))
+        return Response(Exam.objects.get(key=key))
 
-class Query(ApiView):
+class Query(APIView):
     def get(self, request, format=None):
-        return Response(Exams.objects.filter(description__contains=request.GET.get('query')))
+        return Response(Exam.objects.filter(description__contains=request.GET.get('query')))
 
-class ExamModify(ApiView):
+class ExamModify(APIView):
     def post(self, request, key, format=None):
-        exam = Exams.objects.get(key=key)
+        exam = Exam.objects.get(key=key)
         exam.description = request.GET.get('description')
         exam.save()
         return Response(status=status.HTTP_200_OK)
@@ -28,12 +37,12 @@ class GradesList(generics.ListCreateAPIView):
     queryset = StudentExam.objects.all()
     serializer_class = GradeSerializer
 
-class ExamDelete(ApiView):
+class ExamDelete(APIView):
     def delete(self, request, key, format=None):
-        exam = Exams.objects.get(key=key)
+        exam = Exam.objects.get(key=key)
         students_for_this_exam = StudentExam.objects.filter(exam=exam)
         for student in students_for_this_exam:
             if student['grade']:
                 return Response(status=status.HTTP_400) # ??
         exam.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT
+        return Response(status=status.HTTP_204_NO_CONTENT)
