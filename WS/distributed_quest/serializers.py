f1589b499e6be6e0146bc8bc38aaffd123f77441
josemibnf <josemi.bnf@outlook.es>
2021-01-09 13:14:42 +0100

    serializer

--- WS/distributed_quest/serializers.py
+++ WS/distributed_quest/serializers.py
@@ -1,15 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
-class ExamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Exam
-        fields = '__all__' 
-
+class ExamSerializer(serializers.Serializer):
     description = serializers.CharField(max_length=MAX_DESCRIPTION_LENGTH)
     date = serializers.DateField(required=False, format="%d-%m-%Y",)
-    time = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S")
+    time = serializers.DateTimeField(required=False, format="%Y-%m-%dT%H:%M:%S")
     location = serializers.CharField(max_length=MAX_UBICATION_LENGTH)
+
+    def create(self, validated_data):
+        return Exam.objects.create(**validated_data)
 
 class GradeSerializer(serializers.Serializer):
     class Meta:
